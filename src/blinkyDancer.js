d68940a56e26ccc6d47eb4380f6737227dfe7d08
Hack Reactor Student <student@hackreactor.com>
2019-04-16 15:08:57 -0700

    Fix pseudoclassical inheritance relationship

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,31 +1,12 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-  this.timeBetweenSteps = timeBetweenSteps;
-  // this.step = function() {
-  //   // call the old version of step at the beginning of any call to this new version of step
-  //   oldStep();
-  //   // toggle() is a jQuery method to show/hide the <span> tag.
-  //   // See http://api.jquery.com/category/effects/ for this and
-  //   // other effects you can use on a jQuery-wrapped html tag.
-  //   this.$node.toggle();
-  //   // Bind makes sure the setTimeout keeps the reference to the 
-  //   // instance for future calls of step
-  //   setTimeout(this.step.bind(this), timeBetweenSteps);
-  // };
-  this.step();
+  this.danceMove = function() {
+    this.$node.toggle();
+  };
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
-BlinkyDancer.prototype.step = function() {
-  // call the old version of step at the beginning of any call to this new version of step
-  Dancer.prototype.step();
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
-  // Bind makes sure the setTimeout keeps the reference to the 
-  // instance for future calls of step
-  setTimeout(this.step.bind(this), this.timeBetweenSteps);
-};
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+// BlinkyDancer.prototype.blink = function() {
+//   this.$node.toggle();
+// };
