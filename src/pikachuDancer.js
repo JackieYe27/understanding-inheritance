11c33215959b34dad464b00e643d165798a7a138
Hack Reactor Student <student@hackreactor.com>
2019-04-16 15:09:27 -0700

    Add animated pikachu dancer

--- src/pikachuDancer.js
+++ src/pikachuDancer.js
@@ -1,7 +1,28 @@
 var PikachuDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<img class="dancer pikachu" src="resources/pikachu.png"></img>');
+  this.$node = $('<img class="pikachu" src="resources/pikachu.png"></img>');
   this.setPosition(top, left);
+  this.dancePhase = false;
+  this.danceMove = function() {
+    if (this.dancePhase) {
+      this.$node.animate(
+      {
+        height: '150px',
+        width: '50px',
+      },
+      timeBetweenSteps);
+      this.dancePhase = false;
+    } else {
+      this.$node.animate(
+        {
+          height: '50px',
+          width: '150px',
+        },
+      timeBetweenSteps);
+      this.dancePhase = true;
+    }
+  };
 };
 
 PikachuDancer.prototype = Object.create(Dancer.prototype);
+PikachuDancer.prototype.constructor = PikachuDancer;
