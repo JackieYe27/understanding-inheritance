0cfb729d6a35af9f93ff1579972bc1f7cfe6b3a1
Hack Reactor Student <student@hackreactor.com>
2019-04-16 20:34:30 -0700

    Makes 3D dancer background transparent

--- src/threeDancer.js
+++ src/threeDancer.js
@@ -1,8 +1,11 @@
 var threePartyStarter = function() {
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(75, window.innerWidth/(window.innerHeight/6), 0.1, 1000);
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight/6);
+  var camera = new THREE.PerspectiveCamera(75, (window.innerWidth/6)/(window.innerHeight/6), 0.1, 1000);
+  var renderer = new THREE.WebGLRenderer({
+    alpha: true
+  });
+  renderer.setClearColor( 0xffffff, 0);
+  renderer.setSize(window.innerWidth/6, window.innerHeight/6);
   document.body.appendChild(renderer.domElement);
 
   var light = new THREE.PointLight(0xffffff, 2, 100);
@@ -12,13 +15,13 @@ var threePartyStarter = function() {
   lightTwo.position.set(3, 1, 2);
   scene.add(lightTwo);
 
-  var bgPlane = new THREE.PlaneGeometry(100, 100, 16);
-  var bgMat = new THREE.MeshStandardMaterial({
-    color: 0xf1f442,
-    roughness: 1
-  });
-  var plane = new THREE.Mesh(bgPlane, bgMat);
-  scene.add(plane);
+  // var bgPlane = new THREE.PlaneGeometry(100, 100, 16);
+  // var bgMat = new THREE.MeshStandardMaterial({
+  //   color: 0xf1f442,
+  //   roughness: 1
+  // });
+  // var plane = new THREE.Mesh(bgPlane, bgMat);
+  // scene.add(plane);
 
   var geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
   var material = new THREE.MeshStandardMaterial({
